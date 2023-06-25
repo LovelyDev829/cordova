b45757ff62ce0c6897d8b327400fdbdeae3f65db
LovelyDev829 <lovelydeveloper829@gmail.com>
2023-06-26 08:55:29 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    serverRuntimeConfig: {
+      // Will only be available on the server side
+      mySecret: 'secret',
+      port: process.env.PORT || 3000,
+    },
+    publicRuntimeConfig: {
+      // Will be available on both server and client
+      staticFolder: '/static',
+    },
+  }
 
 module.exports = nextConfig
